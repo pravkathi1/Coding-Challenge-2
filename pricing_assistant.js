c531f0607a787aa83eae4f5b365bad637a62f9a0
Pravallika <pkathi@usf.edu>
2025-09-07 12:27:30 -0400

    commit changes

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -15,8 +15,8 @@ breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit)
 isProfitablePerUnit = profitPerUnit > 0
 
 console.log("Product:" , productName);
-console.log("Discounted Price",  discountedPrice);
-console.log("Final Price With Tax" , finalPriceWithTax);
-console.log("Profit Per Unit" , finalPriceWithTax - costPerUnit);
-console.log("Break Even Units" , breakEvenUnits);
+console.log("Discounted Price:", "$" + discountedPrice);
+console.log("Final Price With Tax:" , "$" + finalPriceWithTax);
+console.log("Profit Per Unit:" , "$" + finalPriceWithTax - costPerUnit);
+console.log("Break Even Units:" , "$" + breakEvenUnits);
 console.log("Profitable" , isProfitablePerUnit);
