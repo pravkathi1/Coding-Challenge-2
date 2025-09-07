cd38cf59c9bd73855ee9e14f72c552746c937612
Pravallika <pkathi@usf.edu>
2025-09-07 16:48:38 -0400

    commit changes

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -17,6 +17,6 @@ isProfitablePerUnit = profitPerUnit > 0
 console.log("Product:" , productName);
 console.log("Discounted Price:", "$" + discountedPrice);
 console.log("Final Price With Tax:" , "$" + finalPriceWithTax);
-console.log("Profit Per Unit:" , "$" + finalPriceWithTax - costPerUnit);
+console.log("Profit Per Unit:" , "$" + (finalPriceWithTax - costPerUnit));
 console.log("Break Even Units:" , "$" + breakEvenUnits);
 console.log("Profitable" , isProfitablePerUnit);
